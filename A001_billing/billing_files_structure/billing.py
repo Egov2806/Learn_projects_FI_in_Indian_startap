293bb2d30807c695076cbd63762b9d8b3fd76954
Egov2806 <106820430+Egov2806@users.noreply.github.com>
2022-07-13 23:11:00 +0300

    initial commit I got to the visualization, processed each file in the folder in a cycle \ output the first 5 lines \ output the length of the data array \ output info the array \ calculate count null values for columns \ calculate count values for name columns

--- A001_billing/billing_files_structure/billing.py
+++ A001_billing/billing_files_structure/billing.py
@@ -1,2 +1,15 @@
 import pandas as pd
+import os
+import matplotlib.pyplot as plt
 folder_path='C:\\Users\\AlexanderMuntyanu\\Desktop\\ДатаФрейм Индийские Стартапы'
+file_path="C:\\Users\\AlexanderMuntyanu\\Desktop\\ДатаФрейм Индийские Стартапы\\Indian Startups - Funding  Investors Data February 2022.csv"
+for file in os.listdir(folder_path):
+    full_path=folder_path+"\\"+str(file)
+    df=pd.read_csv(full_path, encoding='unicode_escape')
+    #print(df.head(5))
+    print(df.shape)
+    #print(df.info())
+    #print(df.isnull().sum())
+    #print(df.Sector.value_counts().head(10))
+    #print(df.Location.value_counts().head(10))
+    #print(df['Sector'].value_counts().head(10).plot(kind ='pie', figsize = (6,6)))
